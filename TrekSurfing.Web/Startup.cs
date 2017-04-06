4f82a33b63c917d46e80159b01226d69d0fbbc41
Admin <tmikent@gmail.com>
2017-04-06 23:01:38 +0300

    Initial commit

---
+++ TrekSurfing.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TrekSurfing.Web.Startup))]
+namespace TrekSurfing.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
