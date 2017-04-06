f2a9ee6e1995061fc28dfe78febeb89d36359482
Marakaido <yaroslav.mykoliv28@gmail.com>
2017-04-07 00:05:45 +0300

    Add EntityBase, TrekEvent models

---
+++ TrekSurfing.Web/Models/EntityBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TrekSurfing.Web.Models
+{
+    public abstract class EntityBase
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required, DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+        public DateTime Created { get; set; }
+    }
+}
---
+++ TrekSurfing.Web/Models/TrekEvent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TrekSurfing.Web.Models
+{
+    public class TrekEvent
+    {
+        [Required(ErrorMessage = "Name is required")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Start date is required")]
+        public DateTime Starts { get; set; }
+
+        [Required(ErrorMessage = "End date is required")]
+        public DateTime Ends { get; set; }
+
+        public string Description { get; set; }
+
+        [Required(ErrorMessage = "Owner is required")]
+        public ApplicationUser Owner { get; set; }
+
+        [Required(ErrorMessage = "Route is required")]
+        public string Route { get; set; }
+
+        public byte[] Image { get; set; }
+    }
+}
