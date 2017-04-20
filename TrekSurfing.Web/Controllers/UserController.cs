4ce5cb939b310fd84cd02b73aa9a40e272396195
pylypO <ostappp02@gmail.com>
2017-04-21 01:54:50 +0300

    basic profile output

---
+++ TrekSurfing.Web/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using System.Web.Mvc;
+using TrekSurfing.Web.Models;
+using System.Data.Entity;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace TrekSurfing.Web.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: User
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: User/1
+        public ActionResult ViewUser(int id)
+        {
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                ProfileViewModel profile = new ProfileViewModel();
+                var data = (from User in context.Users where id.ToString() == User.Id select new { User.UserName, User.Email, User.PhoneNumber}).ToArray();
+                profile.UserName = data[0].UserName;
+                profile.Email = data[0].Email;
+                profile.PhoneNumber = data[0].PhoneNumber;
+                return View(profile);
+            }
+            
+        }
+    }
+}
---
+++ TrekSurfing.Web/Models/ProfileViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrekSurfing.Web.Models
+{
+    public class ProfileViewModel
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
