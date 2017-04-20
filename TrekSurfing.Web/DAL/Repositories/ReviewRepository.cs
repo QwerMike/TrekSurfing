28db593efd580a85be0b4a37dc2608eee73e1e07
Admim <tmikent@gmail.com>
2017-04-20 23:08:50 +0300

    Add repositories implementation

---
+++ TrekSurfing.Web/DAL/Repositories/ReviewRepository.cs
@@ -0,0 +1,18 @@
+﻿using TrekSurfing.Web.DAL.Interfaces.Repositories;
+using TrekSurfing.Web.Models;
+
+namespace TrekSurfing.Web.DAL.Repositories
+{
+    public class ReviewRepository : Repository<Review>, IReviewRepository
+    {
+        public ReviewRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+
+        public ApplicationDbContext AppDbContext
+        {
+            get { return Context as ApplicationDbContext; }
+        }
+    }
+}
---
+++ TrekSurfing.Web/DAL/Repositories/TrekEventRepository.cs
@@ -0,0 +1,18 @@
+﻿using TrekSurfing.Web.DAL.Interfaces.Repositories;
+using TrekSurfing.Web.Models;
+
+namespace TrekSurfing.Web.DAL.Repositories
+{
+    public class TrekEventRepository : Repository<TrekEvent>, ITrekEventRepository
+    {
+        public TrekEventRepository(ApplicationDbContext context)
+            : base(context)
+        {
+        }
+
+        public ApplicationDbContext AppDbContext
+        {
+            get { return Context as ApplicationDbContext; }
+        }
+    }
+}
