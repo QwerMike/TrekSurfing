253881e60e575eef4429c6c1778ca74c32b5e571
Admim <tmikent@gmail.com>
2017-04-20 23:12:45 +0300

    Replace ApplicationDbContext.cs

---
+++ TrekSurfing.Web/DAL/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System.Data.Entity;
+using TrekSurfing.Web.Models;
+
+namespace TrekSurfing.Web.DAL
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+
+        public DbSet<TrekEvent> TrekEvents { get; set; }
+        public DbSet<Review> Reviews { get; set; }
+    }
+}
