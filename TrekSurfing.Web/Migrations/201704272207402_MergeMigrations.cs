011ec070d249d47d5458b45bac05ebd07ae78120
Admin <tmikent@gmail.com>
2017-04-28 01:49:56 +0300

    Add merge migration

---
+++ TrekSurfing.Web/Migrations/201704272207402_MergeMigrations.cs
@@ -0,0 +1,16 @@
+namespace TrekSurfing.Web.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MergeMigrations : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
