a8aa56aa0cf543a353506ff3d4720e882cc5c661
Posivnych <moroler22@gmail.com>
2017-04-27 23:25:16 +0300

    fix some bugs

--- TrekSurfing.Web.Tests/Controllers/EventControllerTest.cs
+++ TrekSurfing.Web.Tests/Controllers/EventControllerTest.cs
@@ -2,6 +2,8 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using TrekSurfing.Web.Models;
+using TrekSurfing.Web.DAL;
+using TrekSurfing.Web.DAL.Repositories;
 
 namespace TrekSurfing.Web.Tests.Controllers
 {
@@ -11,6 +13,7 @@ namespace TrekSurfing.Web.Tests.Controllers
         [TestMethod]
         public void DeleteEventTest()
         {
+            TrekEvent prod = new TrekEvent { Name = "Test", Starts = DateTime.Now, Ends = DateTime.Now, Route= "testRoute", OwnerId = "1" };
 
         }
     }
