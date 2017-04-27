a57c435c0a45d1c3b1b072a19f616480bac0ef14
Admin <tmikent@gmail.com>
2017-04-28 00:24:37 +0300

    Add RoleAdminController & Migrations for ApplictionRole

---
+++ TrekSurfing.Web/Controllers/RoleAdminController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using TrekSurfing.Web.Infrastructure;
+using TrekSurfing.Web.Models;
+
+namespace TrekSurfing.Web.Controllers
+{
+    public class RoleAdminController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(RoleManager.Roles);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create([Required]string name)
+        {
+            if (ModelState.IsValid)
+            {
+                IdentityResult result 
+                    = await RoleManager.CreateAsync(new ApplicationRole(name));
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            return View(name);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Delete(string id)
+        {
+            ApplicationRole role = await RoleManager.FindByIdAsync(id);
+            if (role != null)
+            {
+                IdentityResult result = await RoleManager.DeleteAsync(role);
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return View("CustomError", result.Errors);
+                }
+            }
+            else
+            {
+                return View("CustomError", new string[] { "Role Not Found" });
+            }
+        }
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (string error in result.Errors)
+            {
+                ModelState.AddModelError("", error);
+            }
+        }
+
+        private ApplicationUserManager UserManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext()
+                    .GetUserManager<ApplicationUserManager>();
+            }
+        }
+
+        private ApplicationRoleManager RoleManager
+        {
+            get
+            {
+                return HttpContext.GetOwinContext()
+                    .GetUserManager<ApplicationRoleManager>();
+            }
+        }
+    }
+
+    public static class IdentityHelpers
+    {
+        public static MvcHtmlString GetUserEmail(this HtmlHelper html, string id)
+        {
+            ApplicationUserManager mgr = 
+                HttpContext.Current.GetOwinContext()
+                .GetUserManager<ApplicationUserManager>();
+
+            return new MvcHtmlString(mgr.FindByIdAsync(id).Result.Email);
+        }
+    }
+}
