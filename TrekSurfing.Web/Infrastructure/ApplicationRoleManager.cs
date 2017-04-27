04928d183e54aaca1173476ed7bd470e949ff5d0
Admin <tmikent@gmail.com>
2017-04-27 21:55:21 +0300

    Add role manager class

---
+++ TrekSurfing.Web/Infrastructure/ApplicationRoleManager.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin;
+using System;
+using TrekSurfing.Web.DAL;
+using TrekSurfing.Web.Models;
+
+namespace TrekSurfing.Web.Infrastructure
+{
+    public class ApplicationRoleManager :
+        RoleManager<ApplicationRole>, IDisposable
+    {
+        public ApplicationRoleManager(RoleStore<ApplicationRole> store)
+            : base(store)
+        {
+        }
+
+        public static ApplicationRoleManager Create(
+            IdentityFactoryOptions<ApplicationRoleManager> options,
+            IOwinContext context)
+        {
+            return new ApplicationRoleManager(
+                new RoleStore<ApplicationRole>(
+                    context.Get<ApplicationDbContext>()));
+        }
+    }
+}
