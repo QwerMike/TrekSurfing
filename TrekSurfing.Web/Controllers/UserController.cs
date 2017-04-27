8241fafaedf2f79a91c5b1c1f04a7f4777517c55
pylypO <ostappp02@gmail.com>
2017-04-27 19:26:17 +0300

    show and edit profile (without frontend)

--- TrekSurfing.Web/Controllers/UserController.cs
+++ TrekSurfing.Web/Controllers/UserController.cs
@@ -8,24 +8,81 @@ namespace TrekSurfing.Web.Controllers
 {
     public class UserController : Controller
     {
-        // GET: User
-        public ActionResult Index()
+
+        /*public ActionResult MyProfile(string id)
         {
             return View();
+        }*/
+
+        // GET: User/ViewProfile/1
+        public ActionResult ViewProfile(string id)
+        {
+            ProfileViewModel profile = getProfileById(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
         }
-        // GET: User/1
-        public ActionResult ViewUser(int id)
+
+        // GET: User/EditProfile/1
+        public ActionResult EditProfile(string id = "0")
+        {
+            ProfileViewModel profile = getProfileById(id);
+            if (profile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(profile);
+        }
+
+        // POST: User/EditProfile/1
+        [HttpPost]
+        public ActionResult EditProfile(ProfileViewModel profile)
+        {
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                if (ModelState.IsValid)
+                {
+                    var user = context.Users.Find(profile.Id);
+
+                    user.UserName = profile.UserName;
+                    user.Email = profile.Email;
+                    user.PhoneNumber = profile.PhoneNumber;
+
+                    context.Entry(user).State = EntityState.Modified;
+                    context.SaveChanges();
+
+                    return RedirectToAction("ViewUser", new { id = profile.Id });
+                }
+                return View(profile);
+            }
+        }
+
+        public ActionResult DeleteProfile(string id)
+        {
+            using (ApplicationDbContext context = new ApplicationDbContext())
+            {
+                return View();
+            }
+        }
+
+        private ProfileViewModel getProfileById(string id)
         {
             using (ApplicationDbContext context = new ApplicationDbContext())
             {
                 ProfileViewModel profile = new ProfileViewModel();
-                var data = (from User in context.Users where id.ToString() == User.Id select new { User.UserName, User.Email, User.PhoneNumber}).ToArray();
-                profile.UserName = data[0].UserName;
-                profile.Email = data[0].Email;
-                profile.PhoneNumber = data[0].PhoneNumber;
-                return View(profile);
+                var data = (from User in context.Users where id == User.Id select new { User.UserName, User.Email, User.PhoneNumber }).First();
+                if (data == null)
+                {
+                    return null;
+                }
+                profile.Id = id;
+                profile.UserName = data.UserName;
+                profile.Email = data.Email;
+                profile.PhoneNumber = data.PhoneNumber;
+                return profile;
             }
-            
         }
     }
 }
--- TrekSurfing.Web/Models/ProfileViewModel.cs
+++ TrekSurfing.Web/Models/ProfileViewModel.cs
@@ -1,14 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace TrekSurfing.Web.Models
 {
     public class ProfileViewModel
     {
+        public string Id { get; set; }
+
+        [Required(ErrorMessage = "Name is required")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "PhoneNumber is required")]
         public string PhoneNumber { get; set; }
+
     }
 }
