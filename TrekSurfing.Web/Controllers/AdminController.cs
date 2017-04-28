90cc3dac3d3acf5716a541ac65f10b705b4c0a5a
Admin <tmikent@gmail.com>
2017-04-28 04:31:38 +0300

    Add delete event by admin

--- TrekSurfing.Web/Controllers/AdminController.cs
+++ TrekSurfing.Web/Controllers/AdminController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNet.Identity.Owin;
 using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using TrekSurfing.Web.DAL.Interfaces;
 using TrekSurfing.Web.Models;
 
 namespace TrekSurfing.Web.Controllers
@@ -10,6 +11,13 @@ namespace TrekSurfing.Web.Controllers
     [Authorize(Roles = "Administrator")]
     public class AdminController : Controller
     {
+        private IUnitOfWork unitOfWork;
+
+        public AdminController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
         public ActionResult Index()
         {
             return View(UserManager.Users);
@@ -35,6 +43,16 @@ namespace TrekSurfing.Web.Controllers
             {
                 return View("CustomError", new string[] { "User Not Found" });
             }
+        }
+
+        [HttpPost]
+        public ActionResult DeleteEvent(int id)
+        {
+            TrekEvent deletedEvent = unitOfWork.TrekEvents.Get(id);
+            unitOfWork.TrekEvents.Remove(deletedEvent);
+            unitOfWork.Complete();
+            TempData["message"] = string.Format("{0} was deleted!", deletedEvent.Name);
+            return RedirectToAction("ViewAllEvents", "Event");
         }
 
         private ApplicationUserManager UserManager
