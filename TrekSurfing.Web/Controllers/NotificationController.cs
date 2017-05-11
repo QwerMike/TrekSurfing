7e9e4cec6b2aa0023f6550d5f79f9f96a522e878
Marakaido <yaroslav.mykoliv28@gmail.com>
2017-05-11 21:17:53 +0300

    Add notifications button

---
+++ TrekSurfing.Web/Controllers/NotificationController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNet.Identity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using TrekSurfing.Web.DAL.Interfaces;
+using TrekSurfing.Web.Models;
+
+namespace TrekSurfing.Web.Controllers
+{
+    public class NotificationController : Controller
+    {
+        private IUnitOfWork unitOfWork;
+
+        public NotificationController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public ActionResult CurrentUserNotifications()
+        {
+            string receiverId = System.Web.HttpContext.Current.User.Identity.GetUserId();
+            IEnumerable<Notification> notifications = unitOfWork.Notifications.GetAll().AsEnumerable();
+            ViewBag.Notifications = notifications;
+            ViewBag.Number = notifications.Count();
+            return PartialView();
+        }
+    }
+}
